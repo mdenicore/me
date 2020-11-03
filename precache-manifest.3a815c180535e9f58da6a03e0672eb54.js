2776d3071c99e3d931320948d785ce8784d2b7d1
Michael Denicore <mdenicor@Michaels-MacBook-Pro.local>
2020-11-03 11:11:14 -0500

    Updates

---
+++ precache-manifest.3a815c180535e9f58da6a03e0672eb54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa9d48de111d3004d355",
+    "url": "/me/static/css/main.c35588f9.chunk.css"
+  },
+  {
+    "revision": "fa9d48de111d3004d355",
+    "url": "/me/static/js/main.fa9d48de.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "7a1f36867c9681b14a1628b94fa35563",
+    "url": "/me/index.html"
+  }
+];
