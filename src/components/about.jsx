6e600c4d9e13274dc5f0ae00ed47fc5d7f45ada5
Michael Denicore <mdenicor@Michaels-MacBook-Pro.local>
2020-11-03 12:32:44 -0500

    profile updates

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -13,8 +13,9 @@ export default class About extends Component {
                     <div className="about-desc">
                     <span className="heading-meta">About Us</span>
                     <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>I am a computer grad student from DAIICT-Gandhinagar with ICT as my major.I love exploring new technologies and often amazed by the progress we as a human species have mad so far in the recent years (apart from the headphone-jack part!!)</p>
-                    <p>I have started reflecting my ideas and thougths through the medium of words recently so spelling and grammer mistaks are very often.You can write me back if you spot any and don't want to live anymore :P </p>
+                    <p>I am an innovative solutions engineer with over 20 years experience providing the development,
+analysis, design, and leadership for the consistent delivery of impactful business enterprise
+solutions. Proven skills in mentoring and leading development teams.</p>
                     </div>
                 </div>
                 </div>
@@ -37,8 +38,8 @@ export default class About extends Component {
                     <i className="icon-bulb" />
                 </span>
                 <div className="desc">
-                    <h3>Web Development </h3>
-                    <p>I have experience building websites and chrome extentions using JavaScript,React,HTML,CSS</p>
+                    <h3>Web Development</h3>
+                    <p>I have experience building web application using Java, JavaScript, React, HTML, CSS</p>
                 </div>
                 </div>
             </div>
@@ -48,8 +49,8 @@ export default class About extends Component {
                     <i className="icon-phone3" />
                 </span>
                 <div className="desc">
-                    <h3>Data Structures & Algorithms</h3>
-                    <p>As coming from the CS background, I have good grasp over fundamental concepts of DSA</p>
+                    <h3>Integrations & Migrations</h3>
+                    <p></p>
                 </div>
                 </div>
             </div>
@@ -59,8 +60,8 @@ export default class About extends Component {
                     <i className="icon-data" />
                 </span>
                 <div className="desc">
-                    <h3>Dev Ops</h3>
-                    <p>I am pursuing my internship with DevOps team at Juniper and working with tools like Jenkins, Docker, K8s</p>
+                    <h3>Leadership</h3>
+                    <p></p>
                 </div>
                 </div>
             </div>
